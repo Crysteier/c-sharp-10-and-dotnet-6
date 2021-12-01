f4e00725c8fce2052450be595875182e98b39048
Guntel <crysteier12345@gmail.com>
2021-12-01 22:24:29 +0100

    Chapter 4 done
    Finally done with this, and it took only 2 hours, good job me!

---
+++ Chapter04/CalculatorLib.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Calculator
+{
+	public Calculator()
+	{
+		public double Add(double a, double b)
+		{
+			return a * b;
+		}
+	}
+}
---
+++ Chapter04/CallStackExceptionHandling/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+WriteLine("In Main");
+Alpha();
+static void Alpha()
+{
+    WriteLine("In Alpha");
+    Beta();
+}
+static void Beta()
+{
+    WriteLine("In Beta");
+    Calculator.Gamma();
+}
