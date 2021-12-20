6704344d80687fc3192681cd54134b596d6fe7f8
Guntel <crysteier12345@gmail.com>
2021-12-20 20:40:23 +0100

    Chapter 9 done, skipped doing the JSON stuff cuz meh

---
+++ Chapter09/WorkingWithSerialization/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Xml.Serialization;
+using WorkingWithSerialization;
+using static System.Console;
+using static System.Environment;
+using static System.IO.Path;
+
+List<Person> people = new()
+{
+    new(30000M)
+    {
+        FirstName = "Alice",
+        LastName = "Smith",
+        DateOfBirth = new(1974, 3, 14)
+    },
+    new(40000M)
+    {
+        FirstName = "Bob",
+        LastName = "Jones",
+        DateOfBirth = new(1969, 11, 23)
+    },
+    new(20000M)
+    {
+        FirstName = "Charlie",
+        LastName = "Cox",
+        DateOfBirth = new(1984, 5, 4),
+        Children = new()
+        {
+            new(0M)
+            {
+                FirstName = "Sally",
+                LastName = "Cox",
+                DateOfBirth = new(2000, 7, 12)
+            }
+        }
+    }
+};
+//create object that will format a list of persons as xml
+XmlSerializer xs = new(people.GetType());
+
+//create file to write to
+string path = Combine(CurrentDirectory, "people.xml");
+using (FileStream stream = File.Create(path))
+{
+    xs.Serialize(stream,people);
+}
+
+WriteLine("Written {0:N0} bytes of XML to {1}",
+ arg0: new FileInfo(path).Length,
+ arg1: path);
+WriteLine();
+// Display the serialized object graph
+WriteLine(File.ReadAllText(path));
+
+using (FileStream xmlLoad = File.Open(path, FileMode.Open))
+{
+    // deserialize and cast the object graph into a List of Person
+    List<Person>? loadedPeople = xs.Deserialize(xmlLoad) as List<Person>;
+    if (loadedPeople is not null)
+    {
+        foreach (Person p in loadedPeople)
+        {
+            WriteLine("{0} has {1} children.",
+            p.LastName, p.Children?.Count ?? 0);
+        }
+    }
+}
